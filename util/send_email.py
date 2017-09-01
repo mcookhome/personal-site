ccdecd1bf2f010b3d3fe4f41bdb77bdbfa71ebf5
Matthew Cook <matthew.cook@wolterskluwer.com>
2017-09-01 13:04:06 -0400

    added tag to email for filtering, as well as removed prints

--- util/send_email.py
+++ util/send_email.py
@@ -4,9 +4,6 @@ def send(from_addr, to_addr, subject, message, password):
     smtp_server = smtplib.SMTP('smtp.gmail.com', 587)
     smtp_server.ehlo()
     smtp_server.starttls()
-    print from_addr
-    print password
     smtp_server.login(from_addr, password)
-    smtp_server.sendmail(from_addr, to_addr, 'Subject: ' + subject + "\n" + message)
-    print "mail"
+    smtp_server.sendmail(from_addr, to_addr, 'Subject:[mcook.me] ' + subject + "\n" + message)
     smtp_server.quit()
