da8d0078f76148e426fc91cce073ee4158b89291
Matthew Cook <mcookhome@gmail.com>
2017-09-07 18:50:42 -0400

    added utf 8 for email

--- util/send_email.py
+++ util/send_email.py
@@ -2,6 +2,10 @@
 
 import smtplib
 
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.header import Header
+
 def send(from_addr, to_addr, subject, message, password):
     """ Sends an email with the given data """
 
@@ -9,5 +13,11 @@ def send(from_addr, to_addr, subject, message, password):
     smtp_server.ehlo()
     smtp_server.starttls()
     smtp_server.login(from_addr, password)
-    smtp_server.sendmail(from_addr, to_addr, 'Subject:[mcook.me] ' + subject + "\n" + message)
+    msg = MIMEMultipart()
+    msg["From"] = from_addr
+    msg["To"] = to_addr
+    msg["Subject"] = "[mcook.me]" + subject
+    msg.attach(MIMEText(message.encode('utf-8'), 'html', 'UTF-8'))
+
+    smtp_server.sendmail(from_addr, to_addr, msg.as_string())
     smtp_server.quit()
