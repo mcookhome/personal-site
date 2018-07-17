5795288afe29fbdd8e3ff5414922456b011af798
Matthew Cook <mcookhome@gmail.com>
2018-07-17 05:51:39 +0000

    Added beginning of spotify page

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 """ Flask app file to run my website! """
 
+import sys
 import random
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, make_response
 from util import credentials, send_email
+from urllib import quote
+import requests
 
 app = Flask(__name__)
 
@@ -24,5 +27,32 @@ def me():
 
     return render_template("me.html", circles=circles, images=images, tab=tab)
 
+@app.route("/billboard-to-spotify", methods=['GET', 'POST'])
+def bts():
+    """ Route to define my billboard-to-spotify page, handling get and post requests """
+    
+    if 'auth_token' not in request.cookies:
+        auth_url = requests.get("http://mcook.me:8080/v1/authorization")
+        return redirect(auth_url.text)
+    elif request.method == 'POST':
+        payload = {}
+        payload["date"] = request.json["date"]
+        payload["chart"] = request.json["chart"]
+        payload["auth_token"] = request.cookies.get('auth_token')
+        print >>sys.stderr, str(payload)
+        requests.post("http://mcook.me:8080/v1/bts", data=payload)
+    else:
+        return render_template("bts.html")
+        
+    
+@app.route("/billboard-to-spotify/callback", methods=['GET', 'POST'])
+def btscallback():
+    """ Route to define my billboard-to-spotify page, handling get and post requests """
+    auth_token = request.args['code']
+    response = make_response(redirect('/billboard-to-spotify'))
+    response.set_cookie('auth_token', auth_token, max_age=60*60)
+    return response
+    
+    
 if __name__ == "__main__":
     app.run(debug=True)
